ba9dd4766d32a4fdcaaca7409448aa2e5379b941
nullmage <jsky.johnson@gmail.com>
2020-09-07 23:53:46 -0600

    Adds backend and frontend init

---
+++ Server/routes/todos.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var router = express.Router();
+const pool = require("../database/db");
+
+//TODO Routes...
+
+// Create a todo
+router.post("/", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// Update a todo
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id
+    ]);
+
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// Read all todos
+router.get("/", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// Read a todo
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+
+
+// Delete a todo
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id
+    ]);
+    res.json("Todo was deleted!");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+//
+module.exports = router;
